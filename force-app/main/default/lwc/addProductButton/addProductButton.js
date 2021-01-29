5ccc4c5a7a2dd3ba13232dd71b73894fca15d472
ahephner <ajhephner178@gmail.com>
2021-01-29 11:26:19 -0500

    calling app areas added button to open new area need to fix area selected markup

--- force-app/main/default/lwc/addProductButton/addProductButton.js
+++ force-app/main/default/lwc/addProductButton/addProductButton.js
@@ -1,12 +1,34 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getAreas from '@salesforce/apex/appProduct.getAreas';
+import {refreshApex} from '@salesforce/apex';
 import { MessageContext, publish} from 'lightning/messageService';
 import Program_Builder from '@salesforce/messageChannel/Program_Builder__c';
 /* https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_salesforce_modules */
 export default class AddProductButton extends LightningElement {
-    
+    @api recordId; 
+    area
     @wire(MessageContext)
         messageContext; 
     
+    @wire(getAreas, {recordId: '$recordId'})
+        areaList
+    //get area options
+    get areaOptions(){
+        //console.log('recordId '+this.recordId);
+        
+        return this.areaList.data; 
+    }
+    
+    selectArea(e){
+        this.area = e.detail.value; 
+        //console.log('area ' + this.area);    
+    }
+
+    newArea(){
+        console.log('clicked');
+        
+    }
+
     openModal(){
         console.log('open modal');
         
