c30cc9f9b62fd4c8e1d74019ab321e79842d9b7d
ahephner <ajhephner178@gmail.com>
2021-01-29 15:51:09 -0500

    adding new areas or selecting areas almost all done. Weird error on setting name on the addProductButton comp. Otherwise done

--- force-app/main/default/lwc/addProductButton/addProductButton.js
+++ force-app/main/default/lwc/addProductButton/addProductButton.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api } from 'lwc';
 import getAreas from '@salesforce/apex/appProduct.getAreas';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import {refreshApex} from '@salesforce/apex';
 import { MessageContext, publish} from 'lightning/messageService';
 import Program_Builder from '@salesforce/messageChannel/Program_Builder__c';
@@ -7,6 +8,8 @@ import Program_Builder from '@salesforce/messageChannel/Program_Builder__c';
 export default class AddProductButton extends LightningElement {
     @api recordId; 
     area
+    titleText = 'Area Selected ';;
+   selectedLabel
     @wire(MessageContext)
         messageContext; 
     
@@ -18,23 +21,44 @@ export default class AddProductButton extends LightningElement {
         
         return this.areaList.data; 
     }
-    
+    //used to select area plus alert the user in the comp. Will use this to verify an area was set before products added
     selectArea(e){
         this.area = e.detail.value; 
-        //console.log('area ' + this.area);    
+        //areaName = e.target.areaOptions.find(opt => opt.value === e.detail.value).label; 
+        //Wrong I was trying to call my areaOptions not what the system has as target.options
+        this.selectedLabel = e.target.options.find(opt => opt.value === e.detail.value).label;
+        this.titleText += this.selectedLabel        
     }
 
     newArea(){
-        console.log('clicked');
+        //open appModal -- parent to child comm example 
+        this.template.querySelector('c-app-modal').openMe();
         
     }
 
     openModal(){
-        console.log('open modal');
-        
-        const payload = {
-            connector: true
-        }
-        publish(this.messageContext, Program_Builder, payload); 
+        //open productTable if area is selected
+        if(this.area){
+            const payload = {
+                connector: true
+            }
+            publish(this.messageContext, Program_Builder, payload); 
+       }else{
+           this.dispatchEvent(
+               new ShowToastEvent({
+                   title: 'Hold on',
+                   message: 'Please first select an area',
+                   variant: 'error',
+               })
+           )
+           
+       }
+    }
+
+
+    addedOption(){
+        //console.log('dad listening');
+        //listen for the child -> appModal to send parent new area has been added
+        return refreshApex(this.areaList)
     }
 }
