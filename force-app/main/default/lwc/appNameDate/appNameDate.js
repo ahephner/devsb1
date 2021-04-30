0b9196df8db97bd361256d178108a7b9aa21ded6
ahephner <ajhephner178@gmail.com>
2021-04-30 14:13:42 -0400

    got the date preview to work..ready to merge

--- force-app/main/default/lwc/appNameDate/appNameDate.js
+++ force-app/main/default/lwc/appNameDate/appNameDate.js
@@ -14,6 +14,8 @@ export default class AppNameDate extends LightningElement {
     custDaysApart
     showPreview = false; 
     preview;
+    nxt;
+    total;
     //dates;
     @track dateRange = [];  
     get numOptions(){
@@ -80,58 +82,44 @@ handleOption(event){
         this.custNumberApps = event.detail.value;
         
     }
-    dateFormat = (appDate) => {
-        //console.log('appDate '+appDate);
-        const app = appDate; 
 
-        const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep','Oct','Nov', 'Dec'];
-        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        const year = app.getFullYear();
-        const date = app.getDate();
-        const monthName = months[app.getMonth()];
-        const dayName = days[app.getDay()];
-        const formated = `${dayName}, ${date} ${monthName} ${year}`
-        //console.log('form ' +formated);
-        return formated;
-    }
     //this is the function that returns the preview string; 
-    buildPreview = (firstDate, repeats, timeBetween, totalApp)=>{
-        const d = new Date(firstDate);
+    //note not great programing but it works....
+buildPreview = (firstDate, repeats, timeBetween, totalApp)=>{
+    const d = new Date(firstDate);
+    this.dateRange = [{id:0, name:firstDate}];
+     console.log('timeBetween '+timeBetween);
+     
+     this.nxt = (repeats * timeBetween)
+     const left = Number(totalApp)-1;
+      this.total = Number(totalApp)+1;  
+     var mult = 1;
+    for (let i = 1; i<= totalApp; i++){
+         d.setDate(d.getDate() + this.nxt)
+         let day = new Date(d); 
+         console.log('day '+d);
          
-         const nxt = (repeats * timeBetween)+1
-         const left = Number(totalApp)-1;
-         const total = Number(totalApp)+1;  
-         var mult = 0;
-        for (let i = 0; i<= repeats; i++){
-             let nxt = (i * timeBetween)
-             d.setDate(d.getDate() + nxt)
-             let day = this.dateFormat(d);
-             this.dateRange=[
-                ...this.dateRange,{
-                   id:mult, name:day
-                }
-            ]
-             mult ++; 
-             //console.log('appDate '+day);
+         this.dateRange=[
+            ...this.dateRange,{
+               id:mult, name:day
+            }
+        ]
+         mult ++; 
+         
 
-        }
-        console.log('dateRange');
-        console.log(this.dateRange);
-        
-        
-        // const message = `The first app is on ${this.dateRange[0].name} `+
-        //                       `the next app will be ${nxt} days after on ${this.dateRange[1].name} `+
-        //                       `and I will make ${left} more applications similar spaced. `+
-        //                       `There will be a total of ${total} created. Remember you can always edit dates after inserting!`
-       // return [message, dateRange]; 
-    }
+    }  
+
+    return this.dateRange
+}
     //this shows preview
     handlePreview(){
         this.showPreview = true;
-       //var call = this.buildPreview(this.appDate,this.custSpreadBetweenApps,this.custDaysApart, this.custNumberApps );
-       this.buildPreview(this.appDate,this.custSpreadBetweenApps,this.custDaysApart, this.custNumberApps );
-        //this.preview = call[0];
-        //this.dates = call.splice(1);
+        this.buildPreview(this.appDate,this.custSpreadBetweenApps,this.custDaysApart, this.custNumberApps );
+        //this is when I was returning multiple values
+        // var call = this.buildPreview(this.appDate,this.custSpreadBetweenApps,this.custDaysApart, this.custNumberApps );
+        // this.preview = call[0];
+        // this.dates = call.splice(1);
+
     }
 
 //flow
@@ -177,69 +165,33 @@ handleOption(event){
     }
 }
 
-//working on updating custom list
-// dateFormat = (appDate) => {
-//       const app = new Date(appDate);
-//       const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep','Oct','Nov', 'Dec'];
-//       const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//       const year = app.getFullYear();
-//       const date = app.getDate();
-//       const monthName = months[app.getMonth()];
-//       const dayName = days[app.getDay()];
-//       const formated = `${dayName}, ${date} ${monthName} ${year}`
-//       //console.log('form ' +formated);
-//       return formated; 
-//     }
-//     preview = (firstDate, repeats, timeBetween, totalApp)=>{
-//      
-//       const d = new Date(firstDate);
-//       firstDate = dateFormat(firstDate)
-//       const x = repeats * timeBetween;
-//       
-//         d.setDate(d.getDate()+ x)
-//       const back = dateFormat(d);
-//       const text = [{id:1,name:'a'}, {id:2,name:'b'}]
-//       const left = totalApp -1
-//       const message = `Your first app will be ${text[0].id} `+
-//                       `and the next app will be ${back} ` +
-//                       `and there will be ${left} number of apps left.`
-//       return [message, 
-//              'item 1',
-//              'item 2'];
-//     }
-//     take2 = (firstDate, repeats, timeBetween, totalApp) =>{
-//              const d = new Date(firstDate);
-//              const dateRange = [];
-//              const left = Number(repeats)-1; 
-//             for (let i = 0; i< repeats; i++){
-//                  let mult = 0;
-//                  let nxt = (i * timeBetween)
-//                  d.setDate(d.getDate() + nxt)
-//                  let appDate = {id:mult, name: dateFormat(d)}
-//                  mult ++; 
-//               dateRange.push(appDate);
-//             }        
-//               const message = `Your first app will be ${dateRange[0].name} `+
-//                       `and the next app will be ${dateRange[1].name} ` +
-//                       `and there will be ${left} number of apps left.`
-//       return[message, dateRange]
-//     }
-//     const x = preview('2021-04-01', 3, 1, 3);  
-//     const y = take2('2021-04-01', 3, 7, 3); 
-//     console.log(y[0])
-//     console.log(y[1])
+// buildPreview = (firstDate, repeats, timeBetween, totalApp)=>{
+//     this.dateRange = [];
+//     const d = new Date(firstDate);
+//      console.log('timeBetween '+timeBetween);
+     
+//      this.nxt = (repeats * timeBetween)
+//      const left = Number(totalApp)-1;
+//       this.total = Number(totalApp)+1;  
+//      var mult = 0;
+//     for (let i = 0; i<= totalApp; i++){
+//          let nxt =  Number(i*timeBetween)
+//          console.log('nxt ' +nxt)
+//          console.log('date '+d);
+         
+//          d.setDate(d.getDate() + nxt)
+//          let day = new Date(d); 
+//          console.log('day '+d);
+         
+//          this.dateRange=[
+//             ...this.dateRange,{
+//                id:mult, name:day
+//             }
+//         ]
+//          mult ++; 
+         
 
+//     }  
 
-        //the getDate is pulling in one day behind expected so I am adding a date. 
-        //what I am updating from build preview
-        // const d = new Date(firstDate);   
-        // const first = new Date(firstDate);
-        // first.setDate(first.getDate()+ 1);
-        // const firstOcc = this.dateFormat(first);  
-        // const nxt = (repeats * timeBetween)+1;
-        // const left = totalApp -1; 
-        // const total = Number(totalApp)+1;  
-        // d.setDate(d.getDate() + nxt)
-        // const nxtDate = this.dateFormat(d);
-        // //const thirdDate = d.setDate(d.getDate()+ (nxt*2));
-        // //thirdDate = this.dateFormat(thirdDate); 
+//     return this.dateRange
+// }
--- force-app/main/default/lwc/dateExamples/dateExamples.js
+++ force-app/main/default/lwc/dateExamples/dateExamples.js
@@ -38,8 +38,8 @@ export default class DateExamples extends LightningElement {
              console.log('appDate '+day);
              this.dateRange.push(appDate);
         }
-        const message = `The first app is on ${this.dateRange[0].name} `+
-        `the next app will be ${nxt} days after on ${this.dateRange[1].name} `+
+        const message = `The first app is on ${firstDate} `+
+        `the next app will be ${nxt} days`+
         `and I will make ${left} more applications similar spaced. `+
         `There will be a total of ${total} created. Remember you can always edit dates after inserting!`
   //here is you can return multiple values. 
@@ -54,3 +54,70 @@ export default class DateExamples extends LightningElement {
         this.dates = call.splice(1);
     }
 }
+
+//working on updating custom list this is for repli.it testing
+// dateFormat = (appDate) => {
+//       const app = new Date(appDate);
+//       const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep','Oct','Nov', 'Dec'];
+//       const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//       const year = app.getFullYear();
+//       const date = app.getDate();
+//       const monthName = months[app.getMonth()];
+//       const dayName = days[app.getDay()];
+//       const formated = `${dayName}, ${date} ${monthName} ${year}`
+//       //console.log('form ' +formated);
+//       return formated; 
+//     }
+//     preview = (firstDate, repeats, timeBetween, totalApp)=>{
+//      
+//       const d = new Date(firstDate);
+//       firstDate = dateFormat(firstDate)
+//       const x = repeats * timeBetween;
+//       
+//         d.setDate(d.getDate()+ x)
+//       const back = dateFormat(d);
+//       const text = [{id:1,name:'a'}, {id:2,name:'b'}]
+//       const left = totalApp -1
+//       const message = `Your first app will be ${text[0].id} `+
+//                       `and the next app will be ${back} ` +
+//                       `and there will be ${left} number of apps left.`
+//       return [message, 
+//              'item 1',
+//              'item 2'];
+//     }
+//     take2 = (firstDate, repeats, timeBetween, totalApp) =>{
+//              const d = new Date(firstDate);
+//              const dateRange = [];
+//              const left = Number(repeats)-1; 
+//             for (let i = 0; i< repeats; i++){
+//                  let mult = 0;
+//                  let nxt = (i * timeBetween)
+//                  d.setDate(d.getDate() + nxt)
+//                  let appDate = {id:mult, name: dateFormat(d)}
+//                  mult ++; 
+//               dateRange.push(appDate);
+//             }        
+//               const message = `Your first app will be ${dateRange[0].name} `+
+//                       `and the next app will be ${dateRange[1].name} ` +
+//                       `and there will be ${left} number of apps left.`
+//       return[message, dateRange]
+//     }
+//     const x = preview('2021-04-01', 3, 1, 3);  
+//     const y = take2('2021-04-01', 3, 7, 3); 
+//     console.log(y[0])
+//     console.log(y[1])
+
+
+        //the getDate is pulling in one day behind expected so I am adding a date. 
+        //what I am updating from build preview
+        // const d = new Date(firstDate);   
+        // const first = new Date(firstDate);
+        // first.setDate(first.getDate()+ 1);
+        // const firstOcc = this.dateFormat(first);  
+        // const nxt = (repeats * timeBetween)+1;
+        // const left = totalApp -1; 
+        // const total = Number(totalApp)+1;  
+        // d.setDate(d.getDate() + nxt)
+        // const nxtDate = this.dateFormat(d);
+        // //const thirdDate = d.setDate(d.getDate()+ (nxt*2));
+        // //thirdDate = this.dateFormat(thirdDate); 
