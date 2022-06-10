99a990ca043b7b32c51a08709c5ed300541f7481
ahephner <ajhephner178@gmail.com>
2022-06-10 15:42:33 -0400

    more calculator build out

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -7,13 +7,47 @@ export default class Calculator extends LightningElement {
     deleteBtn
     allClearBtn
     prevText
-    curtText
+    curtText = 0;
+    useSecond = false; 
     renderedCallback(){
         this.numberButtons = this.template.querySelectorAll('[data-number]');
         this.operationButtons = this.template.querySelectorAll('[data-operation]')
     }
 
-    check(){
-        console.log(this.numberButtons)
+    numbInput(e){
+        if(!this.useSecond){
+            this.curtText = this.curtText === 0 ? '' :this.curtText;
+            console.log(1, this.curtText)
+            this.curtText += e.target.value;
+            console.log(2, this.curtText)  
+        }
+        
+    }
+
+    handleBtn(e){
+        this.operationButtons = e.target.value; 
+        this.prevText = `${this.curtText} ${this.operationButtons}`
+        this.curtText = 0;
+    }
+
+    calc(){
+        let op = this.operationButtons;
+        console.log('this op '+ op)
+        switch (this.operationButtons) {
+            case '+':
+                this.curtText = Number(this.curtText) + Number(this.prevText);
+                break;
+            case '-':
+                this.curtText = Number(this.curtText) - Number(this.prevText);
+                break;
+            case '*':
+                this.curtText = Number(this.curtText) * Number(this.prevText);
+                break;
+            case '/':
+                this.curtText = Number(this.curtText) / Number(this.prevText);
+                break;
+            default:
+                break;
+        }
     }
 }
