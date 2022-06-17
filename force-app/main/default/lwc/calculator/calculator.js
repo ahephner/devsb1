62ee92de9a3446f0f9fded35d0ea48f5ad2ecc19
ahephner <ajhephner178@gmail.com>
2022-06-17 10:41:09 -0400

    calculator logic is done/ Need to resize it a bit

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -7,12 +7,13 @@ export default class Calculator extends LightningElement {
     deleteBtn
     allClearBtn
     prevText
+    prevNumb;
     curtText = 0;
     useSecond = false; 
-    renderedCallback(){
-        this.numberButtons = this.template.querySelectorAll('[data-number]');
-        this.operationButtons = this.template.querySelectorAll('[data-operation]')
-    }
+    // renderedCallback(){
+    //     this.numberButtons = this.template.querySelectorAll('[data-number]');
+    //     this.operationButtons = this.template.querySelectorAll('[data-operation]')
+    // }
 
     numbInput(e){
         if(!this.useSecond){
@@ -26,28 +27,53 @@ export default class Calculator extends LightningElement {
 
     handleBtn(e){
         this.operationButtons = e.target.value; 
+        console.log(this.operationButtons)
         this.prevText = `${this.curtText} ${this.operationButtons}`
+        this.prevNumb = Number(this.curtText)
         this.curtText = 0;
     }
-
+    del(){
+        this.curtText = this.curtText.length > 0 ? this.curtText.substring(0, this.curtText.length-1) : 0; 
+    }
     calc(){
         let op = this.operationButtons;
-        console.log('this op '+ op)
-        switch (this.operationButtons) {
+        //this.prevText = this.prevText.substring(0, this.prevText.length - 2)
+        console.log(typeof this.prevNumb);
+        
+        switch (op) {
             case '+':
-                this.curtText = Number(this.curtText) + Number(this.prevText);
+                this.curtText = Number(this.curtText) + Number(this.prevNumb);
+                this.prevText = this.curtText;
+                this.prevNumb = this.curtText;
                 break;
             case '-':
-                this.curtText = Number(this.curtText) - Number(this.prevText);
+                console.log(1, this.curtText, 2, this.prevNumb)
+                this.curtText = Number(this.prevNumb) - Number(this.curtText);
+                this.prevText = this.curtText;
+                this.prevNumb = this.curtText;
                 break;
             case '*':
-                this.curtText = Number(this.curtText) * Number(this.prevText);
+                this.curtText = Number(this.curtText) * Number(this.prevNumb);
+                this.prevText = this.curtText;
+                this.prevNumb = this.curtText;
                 break;
             case '/':
-                this.curtText = Number(this.curtText) / Number(this.prevText);
+
+                this.curtText = Number(this.prevNumb) / Number(this.curtText);
+                this.prevText = this.curtText;
+                this.prevNumb = this.curtText;
                 break;
             default:
                 break;
         }
+    }
+
+    clear(){
+        console.log('clear');
+        
+        this.curtText = 0;
+        this.prevText = '';
+        this.prevNumb = 0;
+        this.operationButtons = ''; 
     }
 }
