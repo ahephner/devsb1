0dd40884f3745ea8644ad7e3849dbe66aa26b80d
ahephner <ajhephner178@gmail.com>
2023-12-07 12:12:54 -0500

    saving a like query by user

--- force-app/main/default/lwc/privateLike/privateLike.js
+++ force-app/main/default/lwc/privateLike/privateLike.js
@@ -1,13 +1,18 @@
 import { LightningElement, track, wire,api } from 'lwc';
-import { getRecord,updateRecord } from "lightning/uiRecordApi";
+import { getRecord,updateRecord,createRecord } from "lightning/uiRecordApi";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import queryType from '@salesforce/apex/lwcHelper.getRecordTypeId';
 import userId from "@salesforce/user/Id";
 import OWNERID from '@salesforce/schema/Program__c.CreatedById';
 import PUBLISHED from '@salesforce/schema/Program__c.Share_Program__c';
 import LIKE_COUNT from '@salesforce/schema/Program__c.Like_count__c';
 import LIKE from '@salesforce/schema/Program__c.Like__c';
 import PRO_ID from '@salesforce/schema/Program__c.Id';
-//import makePrivate from '@salesforce/schema/'
+//for like statehood
+import QUERY from '@salesforce/schema/Query__c'
+import Q_USER from '@salesforce/schema/Query__c.User__c'
+import Q_PRO_ID from '@salesforce/schema/Query__c.Program__c'
+import Q_RECTYPE from '@salesforce/schema/Query__c.RecordTypeId'
 
 const FIELDS = [OWNERID, PUBLISHED, LIKE_COUNT, LIKE ]
 export default class PrivateLike extends LightningElement {
@@ -19,8 +24,33 @@ export default class PrivateLike extends LightningElement {
     ownerId;
     published;
     showPublish; 
-    showLikes;   
+    showLikes;
+    queryRecordType;  
     
+
+    @wire(queryType, ({objectName: 'Query__c', recTypeName: 'Likes'}))
+    wiredRec({error, data}){
+        if(data){
+            this.queryRecordType = data;
+            console.log(1, this.queryRecordType);
+            
+        }else if(error){
+            let message = "Unknown error";
+            if (Array.isArray(error.body)) {
+                message = error.body.map((e) => e.message).join(", ");
+            } else if (typeof error.body.message === "string") {
+                message = error.body.message;
+            }
+            this.dispatchEvent(
+                new ShowToastEvent({
+                title: "Error loading contact",
+                message,
+                variant: "error",
+                }),
+            );
+        }
+        
+    }
 
 
     @wire(getRecord,{ recordId: "$recordId", fields: FIELDS })
@@ -61,7 +91,24 @@ export default class PrivateLike extends LightningElement {
         fields[PRO_ID.fieldApiName] = this.recordId;
         fields[LIKE_COUNT.fieldApiName] = this.likeCount;
         const addCount = {fields}
-        //updateRecord(addCount); 
+        updateRecord(addCount)
+            .then(()=>{
+            const fields = {};
+            fields[Q_PRO_ID.fieldApiName] = this.recordId;
+            fields[Q_RECTYPE.fieldApiName] = this.queryRecordType;
+            fields[Q_USER.fieldApiName] = this.currentUser;
+            const addLike = {apiName: QUERY.objectApiName, fields}
+            createRecord(addLike)
+                .then((acc)=>{
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                          title: "Liked",
+                          message: "Thanks for the feedback",
+                          variant: "success",
+                        }),
+                      );
+                })
+        }) 
     }
 
     pubProgram(){
@@ -71,6 +118,7 @@ export default class PrivateLike extends LightningElement {
         fields[PRO_ID.fieldApiName] = this.recordId;
         fields[PUBLISHED.fieldApiName] = this.published;
         const pubRec = {fields}
-        updateRecord(pubRec);
+        updateRecord(pubRec)
+           
     }
 }
