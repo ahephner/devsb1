5d4371c1b4f1890e5d4e2430aa96bbf7efc5fcf9
ahephner <ajhephner178@gmail.com>
2023-12-07 15:15:38 -0500

    can only like program once. Creates a new query__c record with user id. Will load user previous likes

--- force-app/main/default/lwc/privateLike/privateLike.js
+++ force-app/main/default/lwc/privateLike/privateLike.js
@@ -1,7 +1,8 @@
 import { LightningElement, track, wire,api } from 'lwc';
-import { getRecord,updateRecord,createRecord } from "lightning/uiRecordApi";
+import { getRecord,updateRecord,createRecord, deleteRecord } from "lightning/uiRecordApi";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import queryType from '@salesforce/apex/lwcHelper.getRecordTypeId';
+import getQueryRecord from '@salesforce/apex/searchQueries.getQueryRecord';
 import userId from "@salesforce/user/Id";
 import OWNERID from '@salesforce/schema/Program__c.CreatedById';
 import PUBLISHED from '@salesforce/schema/Program__c.Share_Program__c';
@@ -18,7 +19,7 @@ const FIELDS = [OWNERID, PUBLISHED, LIKE_COUNT, LIKE ]
 export default class PrivateLike extends LightningElement {
     currentUser = userId;
     @api recordId;  
-    @track likeState = false;
+    @track likeState; 
 
     likeCount;
     ownerId;
@@ -26,13 +27,13 @@ export default class PrivateLike extends LightningElement {
     showPublish; 
     showLikes;
     queryRecordType;  
-    
+    prevLike;
 
     @wire(queryType, ({objectName: 'Query__c', recTypeName: 'Likes'}))
     wiredRec({error, data}){
         if(data){
             this.queryRecordType = data;
-            console.log(1, this.queryRecordType);
+            
             
         }else if(error){
             let message = "Unknown error";
@@ -43,7 +44,7 @@ export default class PrivateLike extends LightningElement {
             }
             this.dispatchEvent(
                 new ShowToastEvent({
-                title: "Error loading contact",
+                title: "Error finding Query Record Type",
                 message,
                 variant: "error",
                 }),
@@ -70,14 +71,37 @@ export default class PrivateLike extends LightningElement {
             }
             this.dispatchEvent(
                 new ShowToastEvent({
-                title: "Error loading contact",
+                title: "Error Like/Publish Info",
                 message,
                 variant: "error",
                 }),
             );
         }
     }
-        
+
+     
+    @wire(getQueryRecord,{userId: '$currentUser', proId: '$recordId'})
+    wiredRecord({data,error}){
+        if(data){
+            let back = data
+            this.prevLike = back.length > 0 ? back: '';
+            this.likeState = back === 'Not Found' ? false : true;  
+        }else if(error){
+            let message = "Unknown error";
+            if (Array.isArray(error.body)) {
+                message = error.body.map((e) => e.message).join(", ");
+            } else if (typeof error.body.message === "string") {
+                message = error.body.message;
+            }
+            this.dispatchEvent(
+                new ShowToastEvent({
+                title: "Error finding prev likes",
+                message,
+                variant: "error",
+                }),
+            );
+        }
+    }  
 
     handleLikeButtonClick(event) {
         //const buttonNumber = event.target.dataset.buttonNumber;
@@ -93,21 +117,38 @@ export default class PrivateLike extends LightningElement {
         const addCount = {fields}
         updateRecord(addCount)
             .then(()=>{
-            const fields = {};
-            fields[Q_PRO_ID.fieldApiName] = this.recordId;
-            fields[Q_RECTYPE.fieldApiName] = this.queryRecordType;
-            fields[Q_USER.fieldApiName] = this.currentUser;
-            const addLike = {apiName: QUERY.objectApiName, fields}
-            createRecord(addLike)
-                .then((acc)=>{
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                          title: "Liked",
-                          message: "Thanks for the feedback",
-                          variant: "success",
-                        }),
-                      );
-                })
+                if(this.likeState){
+                    const fields = {};
+                    fields[Q_PRO_ID.fieldApiName] = this.recordId;
+                    fields[Q_RECTYPE.fieldApiName] = this.queryRecordType;
+                    fields[Q_USER.fieldApiName] = this.currentUser;
+                    const addLike = {apiName: QUERY.objectApiName, fields}
+                    createRecord(addLike)
+                        .then((acc)=>{
+                            this.prevLike = acc.id;
+                            this.dispatchEvent(
+                                new ShowToastEvent({
+                                  title: "Liked",
+                                  message: "Thanks for the feedback",
+                                  variant: "success",
+                                }),
+                              );
+                        })
+                }else if(!this.likeState){
+                    deleteRecord(this.prevLike)
+                    .then(()=>{
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: "Success",
+                                message: "Liked Remove",
+                                variant:"warning"
+                            })
+                        )
+                    })
+                           
+                
+                }
+            
         }) 
     }
 
